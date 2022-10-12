a05e3493ba244a49b20cd7b175c1ffdec18c521f
Al-amin Ahmed <alaacodes@gmail.com>
2022-10-12 10:40:40 -0700

    Task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - execures a function given as a parameter 
+ * array_iterator - execures a function given as a parameter
  * on each element of an array
  * @array: array to iterate over
  * @size: size of the array
