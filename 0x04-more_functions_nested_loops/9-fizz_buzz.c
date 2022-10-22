1e640c51a83e415b2d15210fe2039cfac8c713a2
Al-amin Ahmed <alaacodes>
2022-10-21 23:54:16 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,17 +16,17 @@ int main(void)
 	for (n = 2; n <= 100; n++)
 	{
 		printf(" ");
-		if ((n % 3 == 0))
+		if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		if ((n % 5) == 0)
+		if (n % 5 == 0)
 		{
 			printf("Buzz");
 		}
 		if (n % 3 != 0 && n % 5 != 0)
 		{
-			printf("%d ", n);
+			printf("%d", n);
 		}
 	}
 	printf("\n");
