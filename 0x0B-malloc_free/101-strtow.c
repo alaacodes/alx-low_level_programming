b4a78efb6d191e72e075c7f62ef2f784f070e4ef
Al-amin Ahmed <alaacodes>
2022-10-24 20:35:55 -0700

    Corrections

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,18 +25,14 @@ int _strlen(char *s)
 
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
+ * *str_addChar - adds character to a string
+ * @str: string to add character
+ * @c: character to add to string
+ * Return: size of string after concatenation
  */
 
-
-
-char *str_addChar (char *str, char c)
-
+char *str_addChar(char *str, char c)
 {
-
 	int size, i;
 
 	char *m;
@@ -51,13 +47,13 @@ char *str_addChar (char *str, char c)
 
 	if (m == 0)
 
-			return (0);
+		return (0);
 
 
 
 	for (i = 0; i <= size; i++)
 
-			m[i] = str[i];
+		m[i] = str[i];
 
 
 
@@ -82,12 +78,11 @@ char *str_addChar (char *str, char c)
  */
 
 unsigned int nbr_spaces(char *s)
-
-{	
+{
 	int i, cmpt = 0;
-	
+
 	for (i = 0; s[i + 1] != '\0'; i++)
-	{	
+	{
 		if (s[i]  == ' ' && s[i + 1] != ' ')
 			cmpt++;
 	}
@@ -104,29 +99,17 @@ unsigned int nbr_spaces(char *s)
 char **strtow(char *str)
 
 {
-
 	int i;
-
 	int spaces = nbr_spaces(str);
-
-	char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
-
+	char **tokens = NULL;
 	char *token;
-
 	int checkingSpace = 0;
-
 	int word = 0;
 
-
-
 	if (!tokens)
-
 	{
-
-			printf("Failed");
-
-				return (0);
-
+		printf("Failed");
+		return (0);
 	}
 
 	printf("looping");
@@ -149,12 +132,6 @@ char **strtow(char *str)
 			checkingSpace = 0;
 		}
 	}
-
-
 	tokens[i] = NULL;
-
 	return (tokens);
 }
-
-		
-
