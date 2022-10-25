be2b1b26064c70db18a61242036f1ff5e90aacb4
Al-amin Ahmed <alaacodes>
2022-10-24 21:06:55 -0700

    corrections

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,47 +1,22 @@
 #include "main.h"
-
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
+ * print_number - prints an integer
+ * @n:.input integer parameter
  *
- * Return: address of r or 0
+ *
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_number(int n)
 {
-	int i, j, k, l, m, n;
+	unsigned int i = n;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	if (n < 0)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		_putchar(45);
+		i = -i;
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	if (i / 10)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		print_number(i / 10);
 	}
-	return (r);
+	_putchar(i % 10 + '0');
 }
