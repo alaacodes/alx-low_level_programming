88298512820873f0691b8a41ad63da097da894a1
Al-amin Ahmed <alaacodes>
2022-10-24 18:22:54 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -30,7 +30,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
-		while (lat->next != NULL)
+		while (last->next != NULL)
 			last = last->next;
 		last->next - new;
 	}
